b188f2b1e30e3fe60fac2f370b0701a7eeb5a4cc
Artem Yunda <109692946+iiiAIY@users.noreply.github.com>
2023-10-03 19:22:05 +0300

    Add files via upload

---
+++ firstTask/src/app/services/local-storage.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() { }
+
+  getFormFromLocalStorage(key: string) {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : null;
+  }
+
+  saveFormToLocalStorage (key:string, value: any):void {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  clear() {
+    localStorage.clear();
+  }
+}
